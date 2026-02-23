0623a2602b614970801862621d87fa670e8459bd
kk6523-hub <kk6523@srmist.edu.in>
2026-02-23 12:52:08 +0530

    Add files via upload

---
+++ OOPSBannerApp to join string.java
@@ -0,0 +1,55 @@
+   public class OOPSBannerUC3 {
+    public static void main(String[] args) {
+
+        // Each character is 7 lines high and 9 characters wide
+
+        // Line 1
+        String o1_l1 = " ***   ***";
+        String o2_l1 = " ***   ***";
+        String p_l1  = " *****   ";
+        String s_l1  = " *****   ";
+        System.out.println(String.join("  ", o1_l1, o2_l1, p_l1, s_l1));
+
+        // Line 2
+        String o1_l2 = "*   * *   *";
+        String o2_l2 = "*   * *   *";
+        String p_l2  = "*   *     ";
+        String s_l2  = "*         ";
+        System.out.println(String.join("  ", o1_l2, o2_l2, p_l2, s_l2));
+
+        // Line 3
+        String o1_l3 = "*   * *   *";
+        String o2_l3 = "*   * *   *";
+        String p_l3  = "*   *     ";
+        String s_l3  = "*         ";
+        System.out.println(String.join("  ", o1_l3, o2_l3, p_l3, s_l3));
+
+        // Line 4
+        String o1_l4 = "*   * *   *";
+        String o2_l4 = "*   * *   *";
+        String p_l4  = " *****     ";
+        String s_l4  = " *****     ";
+        System.out.println(String.join("  ", o1_l4, o2_l4, p_l4, s_l4));
+
+        // Line 5
+        String o1_l5 = "*   * *   *";
+        String o2_l5 = "*   * *   *";
+        String p_l5  = "*         ";
+        String s_l5  = "      *    ";
+        System.out.println(String.join("  ", o1_l5, o2_l5, p_l5, s_l5));
+
+        // Line 6
+        String o1_l6 = "*   * *   *";
+        String o2_l6 = "*   * *   *";
+        String p_l6  = "*         ";
+        String s_l6  = "      *    ";
+        System.out.println(String.join("  ", o1_l6, o2_l6, p_l6, s_l6));
+
+        // Line 7
+        String o1_l7 = " ***   ***";
+        String o2_l7 = " ***   ***";
+        String p_l7  = "*         ";
+        String s_l7  = " *****     ";
+        System.out.println(String.join("  ", o1_l7, o2_l7, p_l7, s_l7));
+    }
+}
